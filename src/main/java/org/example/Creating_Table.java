392d6c73c2aa7982fa355e3a3b7b72f88573e2ed
ArtemSoldatovV <solvladislav777@gmail.com>
2025-10-19 11:00:47 +0300

    fixed the construction of the deposit
    fixed the construction of the table and adding it to the file

--- src/main/java/org/example/Creating_Table.java
+++ src/main/java/org/example/Creating_Table.java
@@ -23,19 +23,26 @@ public class Creating_Table {
         int item_number = 1;//номер элемента
         int total_number_of_cells = cells_in_height * cells_in_width;// скольк должно быть ячеек
         int how_many_cells_are_made = 0;// число ячеек
+        int account_cell = 0;//какая ячейка по счёту
         int number_of_width = 0;// число строк
+        int item_number_of_width = 2;
         String exit = table_root( id,  item_number,  y,  x,  cells_in_height,  cells_in_width,  height,  width);
         while (how_many_cells_are_made < total_number_of_cells){
             item_number += 1;
+            if (account_cell == cells_in_width){
+                account_cell = 0;
+            }
 
             if(number_of_width == 0 |
                     how_many_cells_are_made % cells_in_width == 0 & how_many_cells_are_made / cells_in_width == number_of_width){//тут нужна логика для создания строк
+                exit += table_string( id, item_number,  cells_in_height,  height,  width, number_of_width);
+                item_number_of_width = item_number;
                 number_of_width += 1;
-                exit += table_string( id, item_number,  cells_in_height,  height,  width);
             }
             else {
-                exit += table_cell( id,  item_number,  the_value_of_all_cells.get(how_many_cells_are_made),  height,  width);
+                exit += table_cell( id,  item_number,  the_value_of_all_cells.get(how_many_cells_are_made),  height,  width, account_cell, item_number_of_width);
                 how_many_cells_are_made += 1;
+                account_cell += 1;
             }
 
         }
@@ -48,26 +55,36 @@ public class Creating_Table {
         String indent2 = "          ";
         String basics_of_the_table = "\n" + indent1 + "<mxCell id=\"" + id + "-" + item_number + "\" value=\"\" style=\"shape=table;startSize=0;container=1;collapsible=0;childLayout=tableLayout;fontSize=16;\" parent=\"1\" vertex=\"1\">"
                 + "\n" + indent2 + "<mxGeometry x=\"" + x + "\" y=\"" + y + "\" width=\"" + width * cells_in_height + "\" height=\"" + height * cells_in_width + "\" as=\"geometry\" />"
-                + "\n" + indent1 + "</mxCell>\"";
+                + "\n" + indent1 + "</mxCell>";
         return basics_of_the_table;
     }
-    private static String table_string(String id, int item_number, int cells_in_height, int height, int width){
+    private static String table_string(String id, int item_number, int cells_in_height, int height, int width, int number_of_width){
         //строка
         String indent1 = "        ";
         String indent2 = "          ";
-        String basis_of_the_string = "\n" + indent1 + "<mxCell id=\"" + id + "-" + item_number + "\" value=\"\" style=\"shape=tableRow;horizontal=0;startSize=0;swimlaneHead=0;swimlaneBody=0;strokeColor=inherit;top=0;left=0;bottom=0;right=0;collapsible=0;dropTarget=0;fillColor=none;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;fontSize=16;\" parent=\"-r2dthNOID2k0gEMgki2-1\" vertex=\"1\">" +
-                "\n" + indent2 + "<mxGeometry width=\"" + width * cells_in_height + "\" height=\"" + height + "\" as=\"geometry\" />" +
-                "\n" + indent1 + "</mxCell>";
+        String basis_of_the_string = "\n" + indent1 + "<mxCell id=\"" + id + "-" + item_number + "\" value=\"\" style=\"shape=tableRow;horizontal=0;startSize=0;swimlaneHead=0;swimlaneBody=0;strokeColor=inherit;top=0;left=0;bottom=0;right=0;collapsible=0;dropTarget=0;fillColor=none;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;fontSize=16;\" parent=\"" + id +"-1\" vertex=\"1\">" ;
+        if (number_of_width != 0){
+            basis_of_the_string += "\n" + indent2 + "<mxGeometry" + " y=\"" + width * number_of_width + "\" width=\"" + width * cells_in_height + "\" height=\"" + height + "\" as=\"geometry\" />";
+        }
+        else {
+            basis_of_the_string += "\n" + indent2 + "<mxGeometry width=\"" + height * cells_in_height + "\" height=\"" + height + "\" as=\"geometry\" />";
+        }
+        basis_of_the_string += "\n" + indent1 + "</mxCell>";
         return basis_of_the_string;
     }
-    private static String table_cell(String id, int item_number, String cell_value, int height, int width){
+    private static String table_cell(String id, int item_number, String cell_value, int height, int width, int account_cell, int number_of_width){
         //ячейка
         String indent1 = "        ";
         String indent2 = "          ";
         String indent3 = "            ";
-        String base_of_the_cell = "\n" + indent1 + "<mxCell id=\"" + id + "-" + item_number + "\" value=\"(" + cell_value + ")\" style=\"shape=partialRectangle;html=1;whiteSpace=wrap;connectable=0;strokeColor=inherit;overflow=hidden;fillColor=none;top=0;left=0;bottom=0;right=0;pointerEvents=1;fontSize=16;\" parent=\"-r2dthNOID2k0gEMgki2-2\" vertex=\"1\">" +
-                "\n" + indent2 + "<mxGeometry width=\"" + width +"\" height=\"" + height +"\" as=\"geometry\">" +
-                "\n" + indent3 + "<mxRectangle width=\"" + width +"\" height=\"" + height +"\" as=\"alternateBounds\" />" +
+        String base_of_the_cell = "\n" + indent1 + "<mxCell id=\"" + id + "-" + item_number + "\" value=\"(" + cell_value + ")\" style=\"shape=partialRectangle;html=1;whiteSpace=wrap;connectable=0;strokeColor=inherit;overflow=hidden;fillColor=none;top=0;left=0;bottom=0;right=0;pointerEvents=1;fontSize=16;\" parent=\"" + id + "-" + number_of_width + "\" vertex=\"1\">";
+        if (account_cell != 0){
+            base_of_the_cell += "\n" + indent2 + "<mxGeometry " + "x=\"" + width * account_cell + "\" width=\"" + width +"\" height=\"" + height +"\" as=\"geometry\">";
+        }
+        else {
+            base_of_the_cell += "\n" + indent2 + "<mxGeometry width=\"" + width +"\" height=\"" + height +"\" as=\"geometry\">";
+        }
+        base_of_the_cell += "\n" + indent3 + "<mxRectangle width=\"" + width +"\" height=\"" + height +"\" as=\"alternateBounds\" />" +
                 "\n" + indent2 + "</mxGeometry>" +
                 "\n" + indent1 + "</mxCell>";
         return base_of_the_cell;
--- src/main/java/org/example/Writing_to_the_drawio_file.java
+++ src/main/java/org/example/Writing_to_the_drawio_file.java
@@ -11,10 +11,11 @@ public class Writing_to_the_drawio_file {
     public void record (String file_path_and_or_name, String the_billing_table){
         //читаем с каким файлом и работать вставляем таблицу
         try {
+            String where_to_record = "<mxCell id=\"1\" parent=\"0\" />";
             Path path = Paths.get(file_path_and_or_name);//name.drawio
             String content = new String( Files.readAllBytes(path) );
 
-            content = content.replaceAll("<root>", "<root>" + the_billing_table);
+            content = content.replaceAll(where_to_record, where_to_record + the_billing_table);
 
             //запись обратно
             FileWriter writer = new FileWriter(file_path_and_or_name, false);
